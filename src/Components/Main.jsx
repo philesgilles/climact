0d8f8cf68e15ae2c5d17f090c3631f338c9a234d
wailmadrane <wail.madrane@gmail.com>
2019-12-17 15:04:22 +0100

    api call levers

--- src/Components/Main.jsx
+++ src/Components/Main.jsx
@@ -1,22 +1,42 @@
 import React, { useEffect } from 'react'
 import { connect } from 'react-redux'
+import * as queryString from 'query-string'
+
 
 const Main = props => {
 
-   useEffect(async () => {
+   // const urlValueTest = '10101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010101010'
+
+   const apiCall = async () => {
       const response = await fetch('http://54.93.129.246:5000/api/v1.0/levers', { method: 'GET' })
       const fetchedLevers = await response.json()
-      fetchedLevers.forEach(e => {
-         e.value = '00'
-      });
+
+      let urlData = queryString.parse(props.location.search)
+      if (Object.keys(urlData).length > 0) {
+         urlData = urlData.levers.match(/[0-9]{1,2}/g)     
+
+         fetchedLevers.forEach((e, i) => {
+            e.value = urlData[i]
+         });
+      }
+      else {
+         fetchedLevers.forEach(e => {
+            e.value = '00'
+         });
+      }
       console.table(fetchedLevers)
 
       props.setAllLevers(fetchedLevers)
-   },[])
+   }
+
+   useEffect(() => {
+      apiCall()
+   }
+   ,[])
 
    return (
    <div>
-      {props.levers.map((lever, i) => <p key={i}>{lever.value}</p>)}
+      { props.levers.map((lever, i) => <p key={i}>{lever.value}</p>) }
    </div>
    )
 }
