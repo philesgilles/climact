610d196426c2ff687326268a24d64ff8cb7d8c1b
wailmadrane <wail.madrane@gmail.com>
2019-12-18 11:50:51 +0100

    add header julio

--- src/Components/Levers/Pathway.jsx
+++ src/Components/Levers/Pathway.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { PATHWAYS } from '../../utils/constants'
 
 import "./Pathway.css";
 const Pathway = () => {
@@ -7,7 +8,10 @@ const Pathway = () => {
       <h5>Pathway</h5>
       <div>
         <p className="lead">Choose an example pathway</p>
-        <select><option disabled="disabled" value=""></option><option value="0">EU reference</option><option value="1">Key behaviours</option><option value="2">Technology and fuels</option><option value="3">Land-food</option><option value="4">Tech and land-food</option><option value="5">Behaviour and land-food</option><option value="6">Behaviour and tech</option><option value="7">Travel, transport and power</option><option value="8">Homes and buildings</option><option value="9">Past trends</option><option value="10">Middle of the road</option><option value="11">Ambitious</option></select>
+        <select>
+          <option value="" default selected disabled>Choose an example pathway</option>
+          {PATHWAYS.map((pathway, i) => <option value={String(i)} key={i}>{pathway}</option>)}
+        </select>
       </div>
     </div>
   );
