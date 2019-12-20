28be611d2b935836229473f9fda8577b7519cfc7
Philippe Gilles <philgdep@gmail.com>
2019-12-20 16:35:38 +0100

    aa

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -11,22 +11,19 @@ const reducer = (state = initialState, action) => {
         stateLevers: payload
       };
     case "SET_THIS_LEVERS":
-      console.log(payload);
-
       newLevers[payload.id] = {
         ...newLevers[payload.id],
         value: payload.value
       };
       return { stateLevers: newLevers };
     case "SET_GROUP_LEVERS":
-      console.log(payload);
       newLevers = newLevers.map(e => {
         if (e[payload.group] === payload.title) {
           e.value = payload.value;
         }
         return e;
       });
-      console.log(newLevers);
+
       return state;
 
     default:
